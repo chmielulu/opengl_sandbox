65c0ba1bf5effb7e65cb78ef1ebd3907dc1f0f78
Jakub Chmielewski <jakubchmielewski80@gmail.com>
2020-12-30 23:50:43 +0100

    fist commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+extern crate sdl2;
+
+use sdl2::event::*;
+
+fn main() {
+    let sdl = sdl2::init().unwrap();
+    let video_subsystem = sdl.video().unwrap();
+    let _window = video_subsystem
+        .window("Game", 900, 700)
+        .resizable()
+        .build()
+        .unwrap();
+
+    let mut event_pump = sdl.event_pump().unwrap();
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => break 'main,
+                Event::MouseButtonDown { x, y, .. } => {
+                    println!("x: {}, y: {}", x, y);
+                },
+                _ => continue
+            }
+        }
+    }
+}
